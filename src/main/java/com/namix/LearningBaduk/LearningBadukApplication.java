cb26e9e268399c8d99742076e1129db8e56d5af1
Namix <ehdwns6781@naver.com>
2022-04-25 21:15:41 +0900

    Project Start

---
+++ src/main/java/com/namix/LearningBaduk/LearningBadukApplication.java
@@ -0,0 +1,13 @@
+package com.namix.LearningBaduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBadukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBadukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namix/LearningBaduk/LearningBadukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namix.LearningBaduk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBadukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
