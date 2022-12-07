831196693a78aeb04bf888df965130149e9520e9
Namix <ehdwns6781@naver.com>
2022-12-07 20:27:23 +0900

    war package

--- src/main/java/com/namix/LearningBaduk/LearningBadukApplication.java
+++ src/main/java/com/namix/LearningBaduk/LearningBadukApplication.java
@@ -2,12 +2,19 @@ package com.namix.LearningBaduk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:secretApplication.properties" })
-public class LearningBadukApplication {
+public class LearningBadukApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LearningBadukApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LearningBadukApplication.class, args);
 	}
